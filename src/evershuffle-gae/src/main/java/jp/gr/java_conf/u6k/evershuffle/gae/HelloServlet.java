ef23efe1483442bce2c8cecf85fa94284ccbb155
Naono Yuichi <u6k.apps@gmail.com>
2014-10-22 01:40:14 +0900

    OAuthで認証し、ノートブックを列挙する処理を試しに作成した。これをベースにして、機能作成に入ろう。

--- src/evershuffle-gae/src/main/java/jp/gr/java_conf/u6k/evershuffle/gae/HelloServlet.java
+++ src/evershuffle-gae/src/main/java/jp/gr/java_conf/u6k/evershuffle/gae/HelloServlet.java
@@ -1,40 +1,49 @@
 
 package jp.gr.java_conf.u6k.evershuffle.gae;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Properties;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.evernote.auth.EvernoteAuth;
+import com.evernote.auth.EvernoteService;
+import com.evernote.clients.ClientFactory;
+import com.evernote.clients.NoteStoreClient;
+import com.evernote.edam.error.EDAMSystemException;
+import com.evernote.edam.error.EDAMUserException;
+import com.evernote.edam.type.Notebook;
+import com.evernote.thrift.TException;
+
 @SuppressWarnings("serial")
 public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Properties oauthProperties;
-
-        String oauthPropertiesPath = getServletContext().getRealPath("WEB-INF/oauth.properties");
-        FileInputStream fin = new FileInputStream(oauthPropertiesPath);
         try {
-            oauthProperties = new Properties();
-            oauthProperties.load(fin);
-        } finally {
-            fin.close();
-        }
+            EvernoteService evernoteService = EvernoteService.SANDBOX;
+            String accessToken = (String) req.getSession().getAttribute("accessToken");
 
-        resp.setContentType("text/plain");
+            resp.setContentType("text/plain");
+            PrintWriter w = resp.getWriter();
 
-        PrintWriter w = resp.getWriter();
-        try {
-            w.write("consumer_key=" + oauthProperties.getProperty("oauth.consumer_key") + "\n");
-            w.write("consumer_secret=" + oauthProperties.getProperty("oauth.consumer_secret"));
-        } finally {
-            w.flush();
+            EvernoteAuth evernoteAuth = new EvernoteAuth(evernoteService, accessToken);
+            NoteStoreClient noteStoreClient = new ClientFactory(evernoteAuth).createNoteStoreClient();
+
+            List<Notebook> notebookList = noteStoreClient.listNotebooks();
+            for (Notebook notebook : notebookList) {
+                w.write("Notebook: " + notebook.getName() + "\n");
+            }
+        } catch (EDAMUserException e) {
+            throw new ServletException(e);
+        } catch (EDAMSystemException e) {
+            throw new ServletException(e);
+        } catch (TException e) {
+            throw new ServletException(e);
         }
     }
 
